c0e2dcd7de84d09ad3f9fe13626d1afe057e0061
antoinehage <me@antoinehage.com>
2016-03-18 14:43:25 -0400

    some methods do not accept arrays.

--- index.js
+++ index.js
@@ -15,25 +15,28 @@ service.init(function () {
             var extracted = keyword_extractor.extract(text, {
                 remove_digits: true,
                 return_changed_case: true,
+                return_chained_words: true,
                 remove_duplicates: true
             });
+            extracted.sort();
+            extracted = extracted.join(" ");
             return extracted;
         }
     };
 
     service.get("/metrics", function (req, res) {
-        var w1 = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w1);
-        var w2 = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w2);
+        var w1 = lib.extractorino(req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w1);
+        var w2 = lib.extractorino(req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w2);
         var answer = clj_fuzzy.metrics[req.soajs.inputmaskData.m](w1, w2);
         res.jsonp(req.soajs.buildResponse(null, {"answer": answer}));
     });
     service.get("/stemmers", function (req, res) {
-        var w = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w);
+        var w = lib.extractorino(req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w);
         var answer = clj_fuzzy.stemmers[req.soajs.inputmaskData.m](w);
         res.jsonp(req.soajs.buildResponse(null, {"answer": answer}));
     });
     service.get("/phonetics", function (req, res) {
-        var w = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w);
+        var w = lib.extractorino(req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w);
         var answer = clj_fuzzy.phonetics[req.soajs.inputmaskData.m](w);
         res.jsonp(req.soajs.buildResponse(null, {"answer": answer}));
     });

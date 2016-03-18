e4deac403b18ab76c41c165a6cd159263004db02
antoinehage <me@antoinehage.com>
2016-03-18 13:00:16 -0400

    added stopword extractor

--- index.js
+++ index.js
@@ -2,20 +2,39 @@
 var soajs = require('soajs');
 var config = require('./config.js');
 var clj_fuzzy = require('clj-fuzzy');
+var keyword_extractor = require("keyword-extractor");
 
 var service = new soajs.server.service(config);
 service.init(function () {
 
+    var lib = {
+        'extractorino': function (ln, text) {
+            var language = "english";
+            if (ln === "fr")
+                language = "french";
+            var extracted = keyword_extractor.extract(text, {
+                remove_digits: true,
+                return_changed_case: true,
+                remove_duplicates: true
+            });
+            return extracted;
+        }
+    };
+
     service.get("/metrics", function (req, res) {
-        var answer = clj_fuzzy.metrics[req.soajs.inputmaskData.m](req.soajs.inputmaskData.w1, req.soajs.inputmaskData.w2);
+        var w1 = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w1);
+        var w2 = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w2);
+        var answer = clj_fuzzy.metrics[req.soajs.inputmaskData.m](w1, w2);
         res.jsonp(req.soajs.buildResponse(null, {"answer": answer}));
     });
     service.get("/stemmers", function (req, res) {
-        var answer = clj_fuzzy.stemmers[req.soajs.inputmaskData.m](req.soajs.inputmaskData.w);
+        var w = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w);
+        var answer = clj_fuzzy.stemmers[req.soajs.inputmaskData.m](w);
         res.jsonp(req.soajs.buildResponse(null, {"answer": answer}));
     });
     service.get("/phonetics", function (req, res) {
-        var answer = clj_fuzzy.phonetics[req.soajs.inputmaskData.m](req.soajs.inputmaskData.w);
+        var w = lib.extractorino (req.soajs.inputmaskData.ln, req.soajs.inputmaskData.w);
+        var answer = clj_fuzzy.phonetics[req.soajs.inputmaskData.m](w);
         res.jsonp(req.soajs.buildResponse(null, {"answer": answer}));
     });
 
